af31dac26741ff6f171f503ec7b362d86822dacc
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-02-21 19:53:18 -0300

    Update UserService.java

--- src/main/java/tech/noetzold/APItester/service/UserService.java
+++ src/main/java/tech/noetzold/APItester/service/UserService.java
@@ -1,4 +1,12 @@
 package tech.noetzold.APItester.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import tech.noetzold.APItester.repository.UserRepository;
+
+@Service
 public class UserService {
+
+    @Autowired
+    UserRepository userRepository;
 }
