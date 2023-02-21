a60cd3e71cd4c282222b4cdd5ab3163d35f3c2bf
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-02-21 08:29:37 -0300

    Update Requisition.java

--- src/main/java/tech/noetzold/APItester/model/Requisition.java
+++ src/main/java/tech/noetzold/APItester/model/Requisition.java
@@ -1,4 +1,37 @@
 package tech.noetzold.APItester.model;
 
-public class Requisition {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import tech.noetzold.APItester.util.REQ_TYPE;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.io.Serializable;
+import java.util.Calendar;
+import java.util.Map;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name="Requisition")
+public class Requisition implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private Map<String, Object> body;
+
+    private Map<String, Object> parameters;
+
+    @NotNull
+    private REQ_TYPE req_type;
+
+    @Temporal(TemporalType.DATE)
+    @Column(name = "data_cadastro", nullable = false)
+    private Calendar date_request;
+
+    @ManyToOne(cascade=CascadeType.PERSIST)
+    private Result result;
 }
