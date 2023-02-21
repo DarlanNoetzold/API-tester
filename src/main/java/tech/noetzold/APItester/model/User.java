4ed6436d334c314661e2f4e320bb29a3844901c1
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-02-21 08:29:44 -0300

    Update User.java

--- src/main/java/tech/noetzold/APItester/model/User.java
+++ src/main/java/tech/noetzold/APItester/model/User.java
@@ -5,12 +5,13 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import javax.persistence.*;
+import java.io.Serializable;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity(name="User")
-public class User {
+public class User implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
