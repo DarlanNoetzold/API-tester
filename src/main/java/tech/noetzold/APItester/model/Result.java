d2c858f5a462754da73720105a4537b0fcf41490
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-02-21 08:29:42 -0300

    Update Result.java

--- src/main/java/tech/noetzold/APItester/model/Result.java
+++ src/main/java/tech/noetzold/APItester/model/Result.java
@@ -1,4 +1,30 @@
 package tech.noetzold.APItester.model;
 
-public class Result {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import tech.noetzold.APItester.util.REQ_STATUS;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import java.io.Serializable;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name="Result")
+public class Result implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @NotNull
+    private REQ_STATUS req_status;
+
+    @NotNull
+    private String details;
 }
