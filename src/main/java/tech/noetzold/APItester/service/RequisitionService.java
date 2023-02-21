aa93f678628fdda60d3f42ba7793eb354fe5108e
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-02-21 19:53:14 -0300

    Update RequisitionService.java

--- src/main/java/tech/noetzold/APItester/service/RequisitionService.java
+++ src/main/java/tech/noetzold/APItester/service/RequisitionService.java
@@ -1,4 +1,12 @@
 package tech.noetzold.APItester.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import tech.noetzold.APItester.repository.RequisitionRepository;
+
+@Service
 public class RequisitionService {
+
+    @Autowired
+    RequisitionRepository requisitionRepository;
 }
