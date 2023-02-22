680ccd3bfd511da607acafa398d89d6d0ba27f1f
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-02-22 13:41:58 -0300

    Update ResultService.java

--- src/main/java/tech/noetzold/APItester/service/ResultService.java
+++ src/main/java/tech/noetzold/APItester/service/ResultService.java
@@ -2,6 +2,8 @@ package tech.noetzold.APItester.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import tech.noetzold.APItester.model.Requisition;
+import tech.noetzold.APItester.model.Result;
 import tech.noetzold.APItester.repository.ResultRepository;
 
 @Service
@@ -9,4 +11,9 @@ public class ResultService {
 
     @Autowired
     ResultRepository resultRepository;
+
+    public Result saveService(Result result){
+        return resultRepository.save(result);
+    }
+
 }
