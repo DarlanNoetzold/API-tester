f30a2f531970abd7e5ba8ab65a1cbb1d6fc72c34
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-02-22 13:41:50 -0300

    Update Requisition.java

--- src/main/java/tech/noetzold/APItester/model/Requisition.java
+++ src/main/java/tech/noetzold/APItester/model/Requisition.java
@@ -9,6 +9,7 @@ import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 import java.util.Calendar;
+import java.util.List;
 import java.util.Map;
 
 @Data
@@ -21,9 +22,9 @@ public class Requisition implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    private Map<String, Object> body;
+    private String body;
 
-    private Map<String, Object> parameters;
+    private String parameters;
 
     @NotNull
     private REQ_TYPE req_type;
@@ -32,6 +33,13 @@ public class Requisition implements Serializable {
     @Column(name = "data_cadastro", nullable = false)
     private Calendar date_request;
 
-    @ManyToOne(cascade=CascadeType.PERSIST)
-    private Result result;
+    @OneToMany(cascade=CascadeType.PERSIST)
+    private List<Result> result;
+
+    public Requisition(Map<String, String> parameters, REQ_TYPE req_type, Calendar date_request, List<Result> result) {
+        this.parameters = parameters.toString();
+        this.req_type = req_type;
+        this.date_request = date_request;
+        this.result = result;
+    }
 }
