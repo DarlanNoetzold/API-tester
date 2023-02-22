3b815e6c3d6e71881214c8b5044dbd57bc69e8f0
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-02-22 13:42:00 -0300

    Update UserService.java

--- src/main/java/tech/noetzold/APItester/service/UserService.java
+++ src/main/java/tech/noetzold/APItester/service/UserService.java
@@ -2,11 +2,27 @@ package tech.noetzold.APItester.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import tech.noetzold.APItester.model.User;
 import tech.noetzold.APItester.repository.UserRepository;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
 
     @Autowired
     UserRepository userRepository;
+
+    public List<User> findAllUsuarios(){
+        return userRepository.findAll();
+    }
+
+    public User saveUsuario(User user){
+        return userRepository.save(user);
+    }
+
+    public Optional<User> validateLogin(String login){
+        return userRepository.findByLogin(login);
+    }
 }
