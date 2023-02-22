ae2b1617765df8cb01762e7b869f1d075b37be59
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-02-22 13:41:46 -0300

    Update ApiTesterApplication.java

--- src/main/java/tech/noetzold/APItester/ApiTesterApplication.java
+++ src/main/java/tech/noetzold/APItester/ApiTesterApplication.java
@@ -2,12 +2,21 @@ package tech.noetzold.APItester;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class ApiTesterApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiTesterApplication.class, args);
+	}
+
+	@Bean
+	public PasswordEncoder getPasswordEncoder() {
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		return encoder;
 	}
 
 }
