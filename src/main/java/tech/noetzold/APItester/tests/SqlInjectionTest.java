bd163be9acb7ea5981fbc6441a405f165671bc2e
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-02-23 13:02:11 -0300

    Update SqlInjectionTest.java

--- src/main/java/tech/noetzold/APItester/tests/SqlInjectionTest.java
+++ src/main/java/tech/noetzold/APItester/tests/SqlInjectionTest.java
@@ -2,23 +2,32 @@ package tech.noetzold.APItester.tests;
 
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
+import org.springframework.web.util.UriComponentsBuilder;
 import tech.noetzold.APItester.model.Result;
 import tech.noetzold.APItester.util.TEST_TYPE;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 import java.util.Map;
 
 public class SqlInjectionTest extends BaseTest {
 
     public Result testGetSqlInjection(String url, RequestSpecification request, Map<String,String> params) {
-        if(params == null) return null;
+        if (params == null) return null;
         String payload = "' or 1=1 --";
-        for (Map.Entry<String,String> pair : params.entrySet())
-            pair.setValue(payload);
+        try {
+            payload = URLEncoder.encode(payload, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException(e);
+        }
+
+        UriComponentsBuilder uriBuilder = UriComponentsBuilder.fromHttpUrl(url);
+        for (Map.Entry<String, String> pair : params.entrySet()) {
+            uriBuilder.queryParam(pair.getKey(), payload);
+        }
 
         Response response = request
-                .params(params)
-                .when()
-                .get(url)
+                .get(uriBuilder.toUriString())
                 .then()
                 .extract()
                 .response();
@@ -26,10 +35,10 @@ public class SqlInjectionTest extends BaseTest {
         String responseBody = response.getBody().asString();
         int statusCode = response.getStatusCode();
         if (responseBody.contains(payload)) {
-            return fail(TEST_TYPE.SQL_INJECTION,"SQL injection vulnerability found in parameter " + params.toString() + " with payload " + payload);
+            return fail(TEST_TYPE.SQL_INJECTION, "SQL injection vulnerability found in parameter " + params.toString() + " with payload " + payload);
         }
         if (statusCode >= 500) {
-            return fail(TEST_TYPE.SQL_INJECTION,"Server error: " + statusCode);
+            return fail(TEST_TYPE.SQL_INJECTION, "Server error: " + statusCode);
         }
 
         return success(TEST_TYPE.SQL_INJECTION);
