a9d863b079be8856009b17c19021635339585b55
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-02-23 13:02:17 -0300

    Update TestPostRequisitionService.java

--- src/main/java/tech/noetzold/APItester/service/TestPostRequisitionService.java
+++ src/main/java/tech/noetzold/APItester/service/TestPostRequisitionService.java
@@ -1,9 +1,11 @@
 package tech.noetzold.APItester.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import tech.noetzold.APItester.model.TestPostRequisition;
 import tech.noetzold.APItester.repository.TestPostRequisitionRepository;
 
+@Service
 public class TestPostRequisitionService {
 
     @Autowired
