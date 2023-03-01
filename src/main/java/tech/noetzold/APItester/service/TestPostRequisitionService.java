966c260d8c23d1fa8084582cd05e43c8ac6b45a1
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-03-01 15:39:37 -0300

    Update TestPostRequisitionService.java

--- src/main/java/tech/noetzold/APItester/service/TestPostRequisitionService.java
+++ src/main/java/tech/noetzold/APItester/service/TestPostRequisitionService.java
@@ -1,6 +1,8 @@
 package tech.noetzold.APItester.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import tech.noetzold.APItester.model.TestPostRequisition;
 import tech.noetzold.APItester.repository.TestPostRequisitionRepository;
@@ -13,5 +15,13 @@ public class TestPostRequisitionService {
 
     public TestPostRequisition saveService(TestPostRequisition testPostRequisition){
         return testPostRequisitionRepository.save(testPostRequisition);
+    }
+
+    public Page<TestPostRequisition> findAll(Pageable pageable){
+        return testPostRequisitionRepository.findAll(pageable);
+    }
+
+    public void deleteGetRequisitionById(Integer id) {
+        testPostRequisitionRepository.deleteById(id);
     }
 }
