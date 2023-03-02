2dfc912478f78429981ef0754983408026c1bc23
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-03-02 11:35:37 -0300

    Update TestGetRequisitionRepository.java

--- src/main/java/tech/noetzold/APItester/repository/TestGetRequisitionRepository.java
+++ src/main/java/tech/noetzold/APItester/repository/TestGetRequisitionRepository.java
@@ -1,7 +1,11 @@
 package tech.noetzold.APItester.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import tech.noetzold.APItester.model.TestGetRequisition;
+import tech.noetzold.APItester.model.User;
 
 public interface TestGetRequisitionRepository extends JpaRepository<TestGetRequisition, Integer> {
+    Page<TestGetRequisition> findByUser(Pageable pageable, User user);
 }
