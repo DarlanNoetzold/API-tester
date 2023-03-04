902d4d321e97be106356ad83dad05df6f0f9e651
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-03-04 10:10:11 -0300

    Update SendToGPT3.java

--- src/main/java/tech/noetzold/APItester/tests/SendToGPT3.java
+++ src/main/java/tech/noetzold/APItester/tests/SendToGPT3.java
@@ -1,4 +1,63 @@
 package tech.noetzold.APItester.tests;
 
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import tech.noetzold.APItester.model.Result;
+import tech.noetzold.APItester.model.TestGetRequisition;
+import tech.noetzold.APItester.model.TestPostRequisition;
+import tech.noetzold.APItester.util.TEST_TYPE;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
 public class SendToGPT3 {
+
+    private TestPostRequisition testPostRequisition;
+
+    private TestGetRequisition testGetRequisition;
+
+    public SendToGPT3(TestPostRequisition testPostRequisition) {
+        this.testPostRequisition = testPostRequisition;
+    }
+
+    public SendToGPT3(TestGetRequisition testGetRequisition) {
+        this.testGetRequisition = testGetRequisition;
+    }
+
+    public Result doGptGetTest(){
+        Map<String, String> bodyToSendRequest = new HashMap<>();
+
+        bodyToSendRequest.put("url", this.testGetRequisition.getUrl());
+        bodyToSendRequest.put("typeReq", "GET");
+        bodyToSendRequest.put("params", this.testGetRequisition.getParameters());
+        bodyToSendRequest.put("headers", this.testGetRequisition.getHeaders());
+        bodyToSendRequest.put("apyKey", this.testGetRequisition.getGptKey());
+
+
+
+        Response responseTestGetGPT = RestAssured.given().body(bodyToSendRequest).post("http://localhost:5000/gptTest").then()
+                .extract()
+                .response();
+
+        return new Result(TEST_TYPE.GPT3, responseTestGetGPT.getBody().toString());
+    }
+
+    public Result doGptPostTest(){
+        Map<String, String> bodyToSendRequest = new HashMap<>();
+
+        bodyToSendRequest.put("url", this.testPostRequisition.getUrl());
+        bodyToSendRequest.put("typeReq", "POST");
+        bodyToSendRequest.put("headers", this.testPostRequisition.getHeaders());
+        bodyToSendRequest.put("body", this.testPostRequisition.getBody());
+        bodyToSendRequest.put("apyKey", this.testPostRequisition.getGptKey());
+
+
+
+        Response responseTestGetGPT = RestAssured.given().body(bodyToSendRequest).post("http://localhost:5000/gptTest").then()
+                .extract()
+                .response();
+
+        return new Result(TEST_TYPE.GPT3, responseTestGetGPT.getBody().toString());
+    }
 }
