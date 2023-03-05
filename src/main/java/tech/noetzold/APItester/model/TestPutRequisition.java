80debf1fb06b548429309f432744b9014f6860f2
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-03-05 17:00:38 -0300

    Create TestPutRequisition.java

---
+++ src/main/java/tech/noetzold/APItester/model/TestPutRequisition.java
@@ -0,0 +1,46 @@
+package tech.noetzold.APItester.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Calendar;
+import java.util.List;
+import java.util.Map;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name="test_put_requisition")
+public class TestPutRequisition implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String body;
+
+    private String url;
+
+    private String headers;
+
+    @Temporal(TemporalType.DATE)
+    @Column(name = "date_request", nullable = false)
+    private Calendar date_request;
+
+    @ManyToOne
+    private User user;
+    @OneToMany(cascade=CascadeType.PERSIST)
+    private List<Result> result;
+
+    private boolean isOnline;
+
+    private String gptKey;
+
+    public TestPutRequisition(Map<String, Object> body, Calendar date_request, List<Result> result, User user) {
+        this.body = body.toString();
+        this.date_request = date_request;
+        this.result = result;
+        this.user = user;
+    }
+}
