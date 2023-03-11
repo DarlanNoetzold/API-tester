0c308aa7c85bb9e2d86d61721df83a43277e16c9
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-03-11 09:36:44 -0300

    Update TestDeleteRequisitionController.java

--- src/main/java/tech/noetzold/APItester/controller/TestDeleteRequisitionController.java
+++ src/main/java/tech/noetzold/APItester/controller/TestDeleteRequisitionController.java
@@ -4,6 +4,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/put")
+@RequestMapping("/delete")
 public class TestDeleteRequisitionController {
 }
