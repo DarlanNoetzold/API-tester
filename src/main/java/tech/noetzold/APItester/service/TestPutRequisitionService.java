9493840a9ee4714c69c593b2cc95be7b080b6690
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-03-11 09:36:49 -0300

    Update TestPutRequisitionService.java

--- src/main/java/tech/noetzold/APItester/service/TestPutRequisitionService.java
+++ src/main/java/tech/noetzold/APItester/service/TestPutRequisitionService.java
@@ -1,7 +1,10 @@
 package tech.noetzold.APItester.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import tech.noetzold.APItester.model.TestPutRequisition;
 import tech.noetzold.APItester.repository.TestPutRequisitionRepository;
 
 @Service
@@ -12,4 +15,20 @@ public class TestPutRequisitionService {
 
     @Autowired
     UserService userService;
+
+    public TestPutRequisition saveService(TestPutRequisition testPutRequisition){
+        return testPutRequisitionRepository.save(testPutRequisition);
+    }
+
+    public Page<TestPutRequisition> findAll(Pageable pageable){
+        return testPutRequisitionRepository.findAll(pageable);
+    }
+
+    public Page<TestPutRequisition> findByUser(Pageable pageable, String login){
+        return testPutRequisitionRepository.findByUser(pageable, login);
+    }
+
+    public void deleteGetRequisitionById(Integer id) {
+        testPutRequisitionRepository.deleteById(id);
+    }
 }
