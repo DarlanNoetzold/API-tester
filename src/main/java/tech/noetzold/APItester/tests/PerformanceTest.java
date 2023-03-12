5759e770dcffcc251700f86b14cde3ab5129c071
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-03-12 08:04:22 -0300

    Update PerformanceTest.java

--- src/main/java/tech/noetzold/APItester/tests/PerformanceTest.java
+++ src/main/java/tech/noetzold/APItester/tests/PerformanceTest.java
@@ -14,6 +14,8 @@ public class PerformanceTest {
 
     private TestPutRequisition testPutRequisition;
 
+    private TestDeleteRequisition testDeleteRequisition;
+
     public PerformanceTest(TestPostRequisition testPostRequisition) {
         this.testPostRequisition = testPostRequisition;
     }
@@ -25,6 +27,11 @@ public class PerformanceTest {
     public PerformanceTest(TestPutRequisition testPutRequisition) {
         this.testPutRequisition = testPutRequisition;
     }
+
+    public PerformanceTest(TestDeleteRequisition testDeleteRequisition) {
+        this.testDeleteRequisition = testDeleteRequisition;
+    }
+
 
     public List<Result> runPostTests(int numTests, int numRequestsPerTest, Map<String,Object> body, Map<String, String> headers) {
         List<Result> results = new ArrayList<>();
@@ -100,7 +107,75 @@ public class PerformanceTest {
         return new Result(TEST_TYPE.PERFORMANCE, new PerformanceResult(responseTime, maxResponseSize, requestsPerSecond).toString());
     }
 
-    public List<Result> runPutTests(int i, int i1, Map<String, Object> body, Map<String, String> headers) {
-        return runPostTests(i, i1, body, headers);
+    public List<Result> runPutTests(int numTests, int numRequestsPerTest, Map<String,Object> body, Map<String, String> headers) {
+        List<Result> results = new ArrayList<>();
+
+        for (int i = 0; i < numTests; i++) {
+            Result result = runPutTest(numRequestsPerTest, body, headers);
+            results.add(result);
+        }
+
+        return results;
+    }
+
+    public Result runPutTest(int numRequests, Map<String, Object> body, Map<String, String> headers) {
+        Response response = RestAssured.given()
+                .headers(headers)
+                .body(body)
+                .when()
+                .put(testPostRequisition.getUrl());
+
+        if (response.getStatusCode() < 200 || response.getStatusCode() >= 300) {
+            return new Result(TEST_TYPE.PERFORMANCE, "Error " + response.getStatusCode() + " by header " + response.getHeaders() + " by body " + response.getBody());
+        }
+
+        long responseTime = response.getTime();
+
+        int maxResponseSize = response.getBody().asByteArray().length;
+
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < numRequests; i++) {
+            RestAssured.given().headers(headers).body(body).when().post(testPostRequisition.getUrl());
+        }
+        long end = System.currentTimeMillis();
+        double requestsPerSecond = numRequests / ((end - start) / 1000.0);
+
+        return new Result(TEST_TYPE.PERFORMANCE, new PerformanceResult(responseTime, maxResponseSize, requestsPerSecond).toString());
+    }
+
+    public List<Result> runDeleteTests(int numTests, int numRequestsPerTest, Map<String, String> params, Map<String, String> headers) {
+        List<Result> results = new ArrayList<>();
+
+        for (int i = 0; i < numTests; i++) {
+            Result result = runDeleteTest(numRequestsPerTest, params, headers);
+            results.add(result);
+        }
+
+        return results;
+    }
+
+    private Result runDeleteTest(int numRequestsPerTest, Map<String, String> params, Map<String, String> headers) {
+        Response response = RestAssured.given()
+                .headers(headers)
+                .params(params)
+                .when()
+                .delete(testGetRequisition.getUrl());
+
+        if (response.getStatusCode() < 200 || response.getStatusCode() >= 300) {
+            return new Result(TEST_TYPE.PERFORMANCE, "Error " + response.getStatusCode() + " by header " + response.getHeaders() + " by body " + response.getBody());
+        }
+
+        long responseTime = response.getTime();
+
+        int maxResponseSize = response.getBody().asByteArray().length;
+
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < numRequestsPerTest; i++) {
+            RestAssured.given().headers(headers).params(params).when().post(testGetRequisition.getUrl());
+        }
+        long end = System.currentTimeMillis();
+        double requestsPerSecond = numRequestsPerTest / ((end - start) / 1000.0);
+
+        return new Result(TEST_TYPE.PERFORMANCE, new PerformanceResult(responseTime, maxResponseSize, requestsPerSecond).toString());
     }
 }
