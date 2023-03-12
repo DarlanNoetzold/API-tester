f1cb10e081b419ca00518fa4c42608f8b12605e1
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-03-12 08:04:17 -0300

    Update TestDeleteRequisitionService.java

--- src/main/java/tech/noetzold/APItester/service/TestDeleteRequisitionService.java
+++ src/main/java/tech/noetzold/APItester/service/TestDeleteRequisitionService.java
@@ -1,7 +1,12 @@
 package tech.noetzold.APItester.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import tech.noetzold.APItester.model.TestDeleteRequisition;
+import tech.noetzold.APItester.model.User;
 import tech.noetzold.APItester.repository.TestDeleteRequisitionRepository;
+import tech.noetzold.APItester.repository.UserRepository;
 
 public class TestDeleteRequisitionService {
 
@@ -9,5 +14,22 @@ public class TestDeleteRequisitionService {
     TestDeleteRequisitionRepository testDeleteRequisitionRepository;
 
     @Autowired
-    UserService userService;
+    UserRepository userRepository;
+
+    public TestDeleteRequisition saveService(TestDeleteRequisition testDeleteRequisition){
+        return testDeleteRequisitionRepository.save(testDeleteRequisition);
+    }
+
+    public Page<TestDeleteRequisition> findByUser(Pageable pageable, String login){
+        User user = userRepository.findByLogin(login).get();
+        return testDeleteRequisitionRepository.findByUser(pageable, user);
+    }
+
+    public Page<TestDeleteRequisition> findAll(Pageable pageable){
+        return testDeleteRequisitionRepository.findAll(pageable);
+    }
+
+    public void deleteDeleteRequisitionById(Integer id) {
+        testDeleteRequisitionRepository.deleteById(id);
+    }
 }
