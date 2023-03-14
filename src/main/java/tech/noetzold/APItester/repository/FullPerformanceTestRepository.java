6aae53d39dfb365eef0d74e22f9ec9ae4303d217
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-03-14 06:44:55 -0300

    Update FullPerformanceTestRepository.java

--- src/main/java/tech/noetzold/APItester/repository/FullPerformanceTestRepository.java
+++ src/main/java/tech/noetzold/APItester/repository/FullPerformanceTestRepository.java
@@ -1,7 +1,11 @@
 package tech.noetzold.APItester.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import tech.noetzold.APItester.model.FullPerformanceTest;
+import tech.noetzold.APItester.model.User;
 
 public interface FullPerformanceTestRepository extends JpaRepository<FullPerformanceTest, Integer> {
+    Page<FullPerformanceTest> findByUser(Pageable pageable, User user);
 }
