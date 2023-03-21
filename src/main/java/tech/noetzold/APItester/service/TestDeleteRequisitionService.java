35fac1d45e8ec260d9b30e92626e05d7d9ccc7b3
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-03-21 06:58:42 -0300

    Update TestDeleteRequisitionService.java

--- src/main/java/tech/noetzold/APItester/service/TestDeleteRequisitionService.java
+++ src/main/java/tech/noetzold/APItester/service/TestDeleteRequisitionService.java
@@ -3,11 +3,13 @@ package tech.noetzold.APItester.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
 import tech.noetzold.APItester.model.TestDeleteRequisition;
 import tech.noetzold.APItester.model.User;
 import tech.noetzold.APItester.repository.TestDeleteRequisitionRepository;
 import tech.noetzold.APItester.repository.UserRepository;
 
+@Service
 public class TestDeleteRequisitionService {
 
     @Autowired
