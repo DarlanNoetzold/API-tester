2db17987d63a2fd130603aea758d244bb7a15a40
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-03-25 17:34:11 -0300

    Create AutomatedRequests.java

---
+++ src/main/java/tech/noetzold/APItester/controller/AutomatedRequests.java
@@ -0,0 +1,9 @@
+package tech.noetzold.APItester.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/automated")
+public class AutomatedRequests {
+}
