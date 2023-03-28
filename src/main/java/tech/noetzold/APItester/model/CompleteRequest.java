ba2ec343e333edf47679e6bd5a7c05e953bb98da
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-03-28 06:28:02 -0300

    Update CompleteRequest.java

--- src/main/java/tech/noetzold/APItester/model/CompleteRequest.java
+++ src/main/java/tech/noetzold/APItester/model/CompleteRequest.java
@@ -5,6 +5,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.io.Serializable;
 import java.util.Calendar;
 import java.util.List;
 
@@ -12,7 +13,7 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity(name="complete_request")
-public class CompleteRequest extends DefaultRequest{
+public class CompleteRequest extends DefaultRequest implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
