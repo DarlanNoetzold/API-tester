54923e600ec52090ded706e379e8c03bb8130e88
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-04-02 10:29:57 -0300

    Update TestPutRequisitionRepository.java

--- src/main/java/tech/noetzold/APItester/repository/TestPutRequisitionRepository.java
+++ src/main/java/tech/noetzold/APItester/repository/TestPutRequisitionRepository.java
@@ -4,7 +4,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import tech.noetzold.APItester.model.TestPutRequisition;
-import tech.noetzold.APItester.model.User;
 
 public interface TestPutRequisitionRepository extends JpaRepository<TestPutRequisition, Integer> {
     Page<TestPutRequisition> findByUser(Pageable pageable, String login);
