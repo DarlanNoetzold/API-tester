e3a1b7526cd489a71087d5f0015715855ece6446
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-04-02 10:29:59 -0300

    Update BaseTest.java

--- src/main/java/tech/noetzold/APItester/tests/BaseTest.java
+++ src/main/java/tech/noetzold/APItester/tests/BaseTest.java
@@ -2,10 +2,6 @@ package tech.noetzold.APItester.tests;
 
 import tech.noetzold.APItester.model.Result;
 import tech.noetzold.APItester.util.TEST_TYPE;
-
-import java.util.HashMap;
-import java.util.Map;
-
 public class BaseTest {
 
     public Result fail(TEST_TYPE test_type, String message){
